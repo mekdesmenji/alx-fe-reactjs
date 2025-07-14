00078a079ccbdbeeb3bd3796c416df20dd78f78b
Mekdes Menji <mekdesmenji79@gmail.com>
2025-07-14 12:48:08 +0300

    other folder added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        textAlign: "center",
+        padding: "20px",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        textAlign: "center",
+        padding: "20px",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
