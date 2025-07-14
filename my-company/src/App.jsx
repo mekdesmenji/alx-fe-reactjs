4184b367831ff68dc2edf6f37eee143d89de6554
Mekdes Menji <mekdesmenji79@gmail.com>
2025-07-14 14:36:08 +0300

    contact inside the component

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
-import Contact from "./Contact";
+import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 
 function App() {
