20ef617d97812641362c2a049332b62009a0d621
Mekdes Menji <mekdesmenji79@gmail.com>
2025-07-28 09:01:55 +0300

    setRecipes added

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,3 +1,34 @@
+// import { create } from "zustand";
+
+// export const useRecipeStore = create((set) => ({
+//   recipes: [],
+//   favorites: [],
+//   recommendations: [],
+
+//   addRecipe: (recipe) =>
+//     set((state) => ({
+//       recipes: [...state.recipes, recipe],
+//     })),
+
+//   addFavorite: (recipeId) =>
+//     set((state) => ({
+//       favorites: [...state.favorites, recipeId],
+//     })),
+
+//   removeFavorite: (recipeId) =>
+//     set((state) => ({
+//       favorites: state.favorites.filter((id) => id !== recipeId),
+//     })),
+
+//   generateRecommendations: () =>
+//     set((state) => {
+//       const recommended = state.recipes.filter(
+//         (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
+//       );
+//       return { recommendations: recommended };
+//     }),
+// }));
+
 import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
@@ -9,6 +40,8 @@ export const useRecipeStore = create((set) => ({
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  setRecipes: (recipes) => set({ recipes }),
 
   addFavorite: (recipeId) =>
     set((state) => ({
