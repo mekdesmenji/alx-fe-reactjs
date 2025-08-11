36e68ba272d3d086992cb59f3ca346221b80b234
Mekdes Menji <mekdesmenji79@gmail.com>
2025-08-11 13:53:23 +0300

    grid-cols-1 added

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ function HomePage() {
         Recipe Sharing Platform
       </h1>
 
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
