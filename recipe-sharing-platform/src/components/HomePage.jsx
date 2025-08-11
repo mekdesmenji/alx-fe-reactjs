786bfe0b942bf90d37cab9801d5d1a3803df514f
Mekdes Menji <mekdesmenji79@gmail.com>
2025-08-11 14:08:37 +0300

    HomePage.jsx does contain react-router-dom

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import recipesData from "../data.json";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   const [recipes, setRecipes] = useState([]);
